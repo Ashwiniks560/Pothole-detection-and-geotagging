c2571dd6f8e66e319e7e4a3841ec0afe5e863579
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-03-29 21:43:02 +0530

    Add files via upload

--- ImageProcessing/IntegratedCode.py
+++ ImageProcessing/IntegratedCode.py
@@ -7,22 +7,22 @@ import numpy as np
 import MapPloting
 
 
-#VideoInput=sys.argv
-#os.system('ffmpeg -i '+str(VideoInput[1])+' -r 1/1 $filename%d.bmp')
+VideoInput=sys.argv
+os.system('ffmpeg -i '+str(VideoInput[1])+' -r 1/1 $filename%d.bmp')
 path=os.getcwd()
-#ImageFiles = [f for f in os.listdir(path) if f.endswith('.bmp')]
+ImageFiles = [f for f in os.listdir(path) if f.endswith('.bmp')]
 
-#for o in ImageFiles:
-#	g="python3 predict.py -c config.json -w trained_wts.h5 -i "+o
-#	os.system(g)
-#	os.remove(o)
-#	files = [i for i in os.listdir(path) if os.path.isfile(os.path.join(path,i)) and 'Part_' in i]
-#	for j in files:
-#		Result = Colour.ColorThreshold(j)
-#		os.remove(j)
-#		if Result:
-#			FileName=os.path.splitext(o)
-#			os.remove(FileName[0]+'_detected.bmp')
+for o in ImageFiles:
+	g="python3 predict.py -c config.json -w trained_wts.h5 -i "+o
+	os.system(g)
+	os.remove(o)
+	files = [i for i in os.listdir(path) if os.path.isfile(os.path.join(path,i)) and 'Part_' in i]
+	for j in files:
+		Result = Colour.ColorThreshold(j)
+		os.remove(j)
+		if Result:
+			FileName=os.path.splitext(o)
+			os.remove(FileName[0]+'_detected.bmp')
 
 ImageDetected= [f for f in os.listdir(path) if f.endswith('.bmp')]
 		 
